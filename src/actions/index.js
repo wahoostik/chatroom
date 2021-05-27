d6aa7e349581a8281ca92f6a0e293cc297b90fed
wahoostik <anthonypaccaud@wanadoo.fr>
2021-05-27 15:12:37 +0200

    création de l'action setInputValue

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+// Action types
+export const SET_INPUT_VALUE = 'SET_INPUT_VALUE';
+
+// Action creators
+export const setInputValue = () => ({
+  type: SET_INPUT_VALUE,
+});
--- src/containers/Form.js
+++ src/containers/Form.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
 /* eslint-disable no-underscore-dangle */
 import { connect } from 'react-redux';
 import Form from 'src/components/Form';
+import { setInputValue } from 'src/actions';
 
 // branchement en lecture du state
 const mapStateToProps = (state) => ({
@@ -11,6 +13,11 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = () => ({
   onChangeInputValue: () => {
     console.log('je veux modifier la valeur de newMessage');
+
+    // pour modifier une valeur du state
+    // il faut créer une action : action types + action creator
+    const action = setInputValue();
+    console.log('action', action);
   },
 });
 
