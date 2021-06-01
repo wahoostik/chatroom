80c4904504d16c645fe466d8909c9d61c915c849
wahoostik <anthonypaccaud@wanadoo.fr>
2021-06-01 18:00:33 +0200

    traitement requête middleware auth

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,9 +1,28 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-const auth = (store) => (next) => (action) => {
-  console.log('state:', store.getState());
-  console.log('action:', action);
+import axios from 'axios';
+import { LOGIN } from 'src/actions';
 
-  next(action);
+const auth = (store) => (next) => (action) => {
+  console.log('middleware authentification');
+  switch (action.type) {
+    case LOGIN: {
+      // à ce niveau là, on veut traiter la requête de connexion
+      console.log('case', LOGIN);
+      axios.post('http://localhost:3001/login', {
+        email: 'walter.white@breakingbad.com',
+        password: 'heisenberg',
+      })
+        .then((response) => {
+          console.log('response', response);
+        })
+        .catch((error) => console.log('error', error));
+      next(action);
+      break;
+    }
+    default:
+      next(action);
+  }
 };
 
 export default auth;
