55d9b6e45e815c5ff69eddcdeeb2b5ed29f03565
wahoostik <anthonypaccaud@wanadoo.fr>
2021-06-03 11:59:00 +0200

    scroll auto à chaque nouveau message

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,16 +1,32 @@
+/* eslint-disable no-console */
+/* eslint-disable max-len */
 // == Import
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 import Message from 'src/containers/Message';
 
 // == Composant
 // On prends l'id du message pour la réconciliation, et on passe tout le reste au composant Message
-const Messages = ({ messages }) => (
-  <div className="messages">
-    {messages.map((theMessage) => <Message key={theMessage.id} {...theMessage} />)}
-  </div>
-);
+const Messages = ({ messages }) => {
+  const messagesRef = useRef(null);
+
+  // On va réagir à chaque rendu du composant mais on va limiter cette réaction au changement de messages
+  useEffect(() => {
+    console.log('éxécution du callback');
+    console.log('scrollTop', messagesRef.current.scrollTop);
+    console.log('scrollHeight', messagesRef.current.scrollHeight);
+    // on vient réassigner le scrollTop à la valeur du scrollHeight
+    // à chaque nouveau message
+    messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+  }, [messages]);
+
+  return (
+    <div className="messages" ref={messagesRef}>
+      {messages.map((theMessage) => <Message key={theMessage.id} {...theMessage} />)}
+    </div>
+  );
+};
 
 Messages.propTypes = {
   messages: PropTypes.arrayOf(
